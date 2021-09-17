ff5dae6a5e597f250d28dea3f4b396f72cfc23da
BGH76 <75952591+BGH76@users.noreply.github.com>
2021-09-17 11:51:14 -0400

    Added JS function

--- js/site.js
+++ js/site.js
@@ -1,4 +1,10 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+
+function getValues() {
+    let userStr = document.getElementById("userString").value;
+    let revString = [];
+    for(let i = userStr.length-1; i >= 0; i--){
+        revString += userStr[i];
+    }
+    document.getElementById("msg").innerHTML = `Your string in reverse is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
