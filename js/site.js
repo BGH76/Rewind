41e4915ff340c83d9092b27740ad490e50d38ea1
BGH76 <75952591+BGH76@users.noreply.github.com>
2021-09-17 12:08:23 -0400

    Added check for string

--- js/site.js
+++ js/site.js
@@ -1,10 +1,15 @@
 
 function getValues() {
     let userStr = document.getElementById("userString").value;
-    let revString = [];
+    if(userStr === ""){
+        alert("Enter a string");
+    }
+    else {
+        let revString = [];
     for(let i = userStr.length-1; i >= 0; i--){
         revString += userStr[i];
     }
     document.getElementById("msg").innerHTML = `Your string in reverse is: ${revString}`;
     document.getElementById("alert").classList.remove("invisible");
+    }
 }
